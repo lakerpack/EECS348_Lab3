0a92952e3d352dbb1a13543b2740107f40a46350
lackerpack <thapaanil2007@gmail.com>
2023-02-19 20:42:12 -0600

    Add input loop

--- Cprogram2/Program2.c
+++ Cprogram2/Program2.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
 int main(){
+    int choice;
+    while (choice > 1){
+        printf("Enter 0 or 1 to STOP \n");
+        printf("Enter the NFL score: ");
+        scanf("%d", &choice);
+    }
     return 0;
 }
