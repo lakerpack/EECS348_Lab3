0679f54aa0f977a8e1c8dcb493117b3338553cb1
lackerpack <thapaanil2007@gmail.com>
2023-02-19 21:14:26 -0600

    Add conditional and finish

--- Cprogram2/Program2.c
+++ Cprogram2/Program2.c
@@ -6,7 +6,9 @@ void printOutcomes(int score){
             for (int touchdown = 0; touchdown * 6 <= score; touchdown++){
                 for (int touchdown_g = 0; touchdown_g * 7 <= score; touchdown_g++){
                     for (int touchdown_c = 0; touchdown_c * 8 <= score; touchdown_c++){
-                        printf("%d TD + 2pt, %d TD + FG, %d TD, %d 3pt FG, %d Safety \n", touchdown_c, touchdown_g, touchdown, goal, safety);
+                        if ((touchdown_c * 8 + touchdown_g * 7 + touchdown * 6 + goal * 3 + safety * 2) == score) {
+                            printf("%d TD + 2pt, %d TD + FG, %d TD, %d 3pt FG, %d Safety \n", touchdown_c, touchdown_g, touchdown, goal, safety);
+                        }
                     }
                 }
             }
