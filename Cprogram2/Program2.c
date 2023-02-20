652555c43db7244cbd0a97828f0649ab8386aa77
lackerpack <thapaanil2007@gmail.com>
2023-02-19 20:50:39 -0600

    Settle structure

--- Cprogram2/Program2.c
+++ Cprogram2/Program2.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
+void printOutcomes(int score){
+    
+}
+
 int main(){
     int choice;
     do {
         printf("Enter 0 or 1 to STOP \n");
         printf("Enter the NFL score: ");
         scanf("%d", &choice);
+        if (choice > 1) printOutcomes(choice);
     } while (choice > 1);
     return 0;
 }
